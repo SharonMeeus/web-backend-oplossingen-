d63ee59d4fe4e0518e2589e6b3d3ba0640916b7a
Sharon Meeus <sharon.meeus@gmail.com>
2015-12-02 21:30:30 +0100

    ongeveer klaar

---
+++ 26. CRUD query/opdracht-crud-query-deel2.php
@@ -0,0 +1,96 @@
+<?php
+	
+	$message = false; // zoals errorMessage bij errorhandling
+	$showtable = false;
+
+	try
+	{
+		$db = new PDO('mysql:host=localhost;dbname=bieren', 'root', 'admin'); // Connectie maken met DB bieren en username rot en password admin
+		$queryString = 'SELECT brouwers.brouwernr, brouwers.brnaam FROM brouwers'; 
+		$statement = $db->prepare($queryString);
+
+		// query uitvoeren
+		$statement->execute();
+
+		//de waarden in een variabele steken.
+		$fetchAssoc = array(); // hierin komt een assoc array van db bieren → (tabelnaam => values)
+
+		while ( $row = $statement->fetch(PDO::FETCH_ASSOC) ) // de waarden uit onze db halen
+		{
+			$fetchAssoc[]	=	$row; // de waarden in de array steken.
+		}
+
+		if(isset($_GET['brouwernr']))
+		{
+			$queryString2 = 'SELECT bieren.naam FROM bieren 
+							 WHERE bieren.brouwernr = :brouwernr';
+
+			$statement2 = $db->prepare($queryString2);
+
+			$statement2->bindValue(':brouwernr', $_GET['brouwernr'] );
+
+			$statement2->execute();
+
+			$fetchBieren = array();
+
+			while($row = $statement2->fetch(PDO::FETCH_ASSOC))
+			{
+				$fetchBieren[] = $row;
+			}
+
+			$showtable = true;
+		}
+
+		// Een query uitvoeren
+		
+
+	}
+	catch(PDOException $e)
+	{
+		$message['type'] = "error";
+		$message['text'] = "Er is iets mis met de database: " . $e->getMessage(); ;
+	}
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>CRUD query - deel 2</title>
+</head>
+<body>
+	<!--<p><?php var_dump($fetchBieren) ?></p>
+	<p><?= var_dump($_GET['brouwernr'])?></p>
+	<p><?= var_dump($message)?></p> -->
+	<p class="fout"><?= ($message) ? $message["text"] : "" ?></p>
+	<form action="opdracht-crud-query-deel2.php" method="get">
+		<select name="brouwernr"> <!-- adhv de name de value van de option getten -->
+			<?php foreach($fetchAssoc as $key => $array) : ?> <!-- je krijgt arrays terug met als key brnaam of brouwernr, we willen de waarden van brnaam in onze option en brouwernr als value om hierdoor te kunnen linken met de brouwernr van bieren -->
+				<option value="<?= $array['brouwernr'] ?>" <?= (($_GET['brouwernr']) == $array['brouwernr']) ? "selected" : "" ?>> <!-- als de gesubmitte brouwernr overeenkomt met de huidige brouwernr vd array → deze selecteren zodat deze zichtbaar blijft -->
+					<?= $array['brnaam'] ?>
+				</option>
+			<?php endforeach ?>
+		</select>
+		<input type="submit" value="Geef mij alle bieren van deze brouwerij" />
+	</form>
+	<?php if($showtable) : ?>
+		<table>
+			<thead>
+			<tr>
+				<?php foreach($fetchBieren[0] as $key => $array) : ?> <!-- kan ook weer [1], [2],...  -->
+					<td><?= $key ?></td> <!-- In dit geval enkel naam -->
+				<?php endforeach ?>
+			</tr>	
+		</thead>
+		<tbody>
+			<?php foreach($fetchBieren as $key => $array) : ?> 
+				<tr>
+					<td><?= $array['naam']?></td> <!-- toch enkel de naam nodig -->
+				</tr>
+			<?php endforeach ?>
+		</tbody>
+		</table>
+	<?php endif ?>
+</body>
+</html>
---
+++ 26. CRUD query/opdracht-crud-query.php
@@ -0,0 +1,66 @@
+<?php
+	
+	$message = false; // zoals errorMessage bij errorhandling
+
+	try
+	{
+		$db = new PDO('mysql:host=localhost;dbname=bieren', 'root', 'admin'); // Connectie maken met DB bieren en username rot en password admin
+		$queryString = 'SELECT * FROM bieren /* selecteer alles van bieren */
+						INNER JOIN brouwers /* gelinkt aan brouwers (zie github document) */
+						ON bieren.brouwernr = brouwers.brouwernr /* gemeenschappelijk id zoeken */
+						WHERE bieren.naam LIKE "du%" /* naam van bier moet beginnen met du → enkel procent op einde */
+						AND brouwers.brnaam LIKE "%a%"'; /* brouwernaam moet a bevatten → procent teken langs beide kanten */
+
+		$statement = $db->prepare($queryString);
+
+		// query uitvoeren
+		$statement->execute();
+
+		//de waarden in een variabele steken. Je hebt zowel de tabelnaam als de values nodig → associative array (tabelnaam => value);
+		$fetchAssoc = array(); // hierin komt een assoc array van db bieren → (tabelnaam => values)
+
+		while ( $row = $statement->fetch(PDO::FETCH_ASSOC) ) // de waarden uit onze db halen
+		{
+			$fetchAssoc[]	=	$row; // de waarden in de array steken.
+		}
+
+	}
+	catch(PDOException $e)
+	{
+		$message['type'] = "error";
+		$message['text'] = "Er is iets mis met de database: " . $e->getMessage(); ;
+	}
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>CRUD query</title>
+</head>
+<body>
+	<h1>Overzicht van de bieren</h1>
+	<!-- <p><?php var_dump($fetchAssoc) ?></p>
+	<p><?= var_dump($error)?></p> -->
+	<p class="fout"><?= ($message) ? $message["text"] : "" ?></p>
+	<table>
+		<thead>
+			<tr>
+				<?php foreach($fetchAssoc[0] as $key => $value) : ?> <!-- mag ook [1], [2],... zijn: Je neemt de eerste array en daarvan altijd de key (=kolomnaam) -->
+					<td><?= $key ?></td>
+				<?php endforeach ?>
+			</tr>	
+		</thead>
+		<tbody>
+			<?php foreach($fetchAssoc as $key => $array) : ?> <!-- $fetchAssoc bestaat uit 4 arrays dus $array is een array → weer foreach </var> -->
+				<tr> <!-- elke array in een nieuwe tr -->
+					<?php foreach($array as $arraykey => $value) : ?> <!-- met $arraykey = 4 keer de kolomnamen, en $value = de waarden → moeten we hebben -->
+						<td><?= $value ?></td>
+					<?php endforeach ?>
+				</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+</body>
+</html>
